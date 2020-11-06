388c2625dcb35b9e9b29e17658f73b6fb799416c
James Hardwick <james.hardwick2@ntlworld.com>
2020-11-06 22:06:34 +0000

    Began working on question 4, imported data sets assinging them new varibles, then added this code into RMarkdown document

--- CW1_script.R
+++ CW1_script.R
@@ -125,8 +125,6 @@ Brentwood_complete_covid_data <-covid_data %>%
   as_tibble() 
 
 # 3.3 ---------------------------------------------------------------------
-# copy of Brentwood_complete_covid_data named Brentwood_day_before 
-Brentwood_day_before 
 
 # load library lubridate
 library("lubridate")
@@ -155,6 +153,19 @@ Brentwood_day_before <- Brentwood_complete_covid_data %>%
 
   
 
+
+# question 4.1 --------------------------------------------------------------
+# import lad19_population data and assigned to a new variable LAD_pop 
+LAD_pop<- readr::read_csv("lad19_population.csv")
+# import COVID19 case data and assigned to a new variable LAD_covid_cases 
+LAD_covid_cases <- readr::read_csv("covid19_cases_20200301_20201017.csv")
+
+
+# need to rename LAD_pop column lad19_area_name to area_name to match
+# LAD_covid_cases table 
+covid_cases_lad_pop <- dplyr::rename(LAD_pop,area_name = lad19_area_name) %>%
+# table join between LAD_pop and LAD_covid_cases 
+dplyr::left_join(LAD_covid_cases, LAD_pop, by = "area_name")
 
 
 
